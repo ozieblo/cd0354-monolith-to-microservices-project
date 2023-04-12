7811ed9d9280500229d26d039446fa24b8320c1f
ozieblo <ozieblo.michal@gmail.com>
2023-04-12 23:04:47 +0200

    multiple corrections

--- set_env.sh
+++ set_env.sh
@@ -2,10 +2,10 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
 export POSTGRES_PASSWORD=postgrescyto
-export POSTGRES_HOST=cdr.c0cohr2xd0rx.us-east-1.rds.amazonaws.com
+export POSTGRES_HOST=postgres.ccgloojg73n3.eu-west-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=test-764782327788-dev
-export AWS_REGION=us-east-1
-export AWS_PROFILE=default
+export AWS_BUCKET=ozieblo-udacity
+export AWS_REGION=eu-west-1
+export AWS_PROFILE=ozieblo
 export JWT_SECRET=testing
 export URL=http://localhost:8100
